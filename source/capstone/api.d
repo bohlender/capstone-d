64ae1983717beb99da72e26658fc55fdbf3e5c00
Dimitri Bohlender <bohlender@embedded.rwth-aachen.de>
2018-10-22 23:53:14 +0200

    Added examples to API documentation

--- source/capstone/api.d
+++ source/capstone/api.d
@@ -111,24 +111,15 @@ struct Detail(Arch arch) {
     }
 }
 
-/// Detail information of disassembled instruction
+/// Disassembled instruction
 struct Instruction(Arch arch) {
-    /// Instruction ID (basically a numeric ID for the instruction mnemonic)
-	InstructionId!arch id;
-    
-    /// Address (EIP) of this instruction
-	ulong address;
-
-	/// Machine bytes of this instruction
-	ubyte[] bytes;
-
-	/// Ascii text of instruction mnemonic
-	string mnemonic;
-
-	/// Ascii text of instruction operands
-	string opStr;
-
-	private Nullable!(Detail!arch) _detail;
+	InstructionId!arch id; /// Instruction ID (basically a numeric ID for the instruction mnemonic)
+	ulong address;         /// Address (EIP) of this instruction
+	ubyte[] bytes;         /// Machine bytes of this instruction
+	string mnemonic;       /// Ascii text of instruction mnemonic
+	string opStr;          /// Ascii text of instruction operands
+	
+    private Nullable!(Detail!arch) _detail;
 
     private this(cs_insn internal, bool detail, bool skipData){
         id = internal.id.to!(InstructionId!arch);
@@ -155,12 +146,12 @@ struct Instruction(Arch arch) {
     }
 }
 
-// TODO: Example for using custom data
-// TODO: See test/skipdata.d for full sample code demonstrating this API.
 /** User-defined callback function type for SKIPDATA mode of operation
  
 The first parameter is the input buffer containing code to be disassembled,
 while the second one holds the position of the currently-examined byte in this buffer.
+
+Returns: The number of bytes to skip, or 0 to immediately stop disassembling
 
 Example:
 ---
@@ -168,8 +159,7 @@ size_t callback(in ubyte[] code, size_t offset) {
     return 2; // Always skip 2 bytes when encountering uninterpretable instructions
 }
 ---
-
-Returns: The number of bytes to skip, or 0 to immediately stop disassembling
+See `setupSkipdata` documentation for full sample code demonstrating this functionality.
 */
 alias Callback = size_t delegate(in ubyte[] code, size_t offset) nothrow @nogc;
 
@@ -205,15 +195,17 @@ auto versionOfLibrary() {
     cs_version(&major, &minor);
     return Version(major, minor);
 }
-
+///
 unittest{
     const libVer = versionOfLibrary;
     const bindVer = versionOfBindings;        
-    assert(libVer == bindVer, "API version mismatch between library (%d.%d) and bindings (%d.%d)"
-        .format(libVer.major, libVer.minor, bindVer.major, bindVer.minor));
+    assert(libVer == bindVer, "API version mismatch between library (%s) and bindings (%s)".format(libVer, bindVer));
 }
 
-/// Indicates whether the installed library was compiled in $(LINK2 http://www.capstone-engine.org/diet.html, diet mode)
+/** Indicates whether the installed library was compiled in $(LINK2 http://www.capstone-engine.org/diet.html, diet mode)
+
+Convenience functionality which is also available via `supports`.
+*/
 auto diet(){
     return supports(SupportQuery.diet);
 }
@@ -224,6 +216,13 @@ Params:
     query = The `SupportQuery` to issue to the library
  
 Returns: True if the requested option is supported
+
+Example:
+---
+// Query installed Capstone library for supported options
+foreach(query; EnumMembers!SupportQuery)
+    writefln!"%-10s: %s"(query, supports(query));
+---
 */
 auto supports(in SupportQuery query){
     return cs_support(query);
@@ -266,8 +265,7 @@ class Capstone(Arch arch){ // Actually parametrised by Registers, InstructionId,
         const libVer = versionOfLibrary;
         const bindVer = versionOfBindings;
         if(libVer != bindVer)
-            throw new CapstoneException("API version mismatch between library (%d.%d) and bindings (%d.%d)"
-                .format(libVer.major, libVer.minor, bindVer.major, bindVer.minor), ErrorCode.UnsupportedVersion);
+            throw new CapstoneException("API version mismatch between library (%s) and bindings (%s)".format(libVer, bindVer), ErrorCode.UnsupportedVersion);
 
         // Create Capstone engine instance
         this._mode = modeFlags;
@@ -278,10 +276,6 @@ class Capstone(Arch arch){ // Actually parametrised by Registers, InstructionId,
         syntax = _syntax;
         detail = _detail;
         skipData = _skipData;
-    }
-    ///
-    unittest{
-        new Capstone!(Arch.x86)(ModeFlags(Mode.bit64));
     }
 
     ~this(){
@@ -330,6 +324,17 @@ class Capstone(Arch arch){ // Actually parametrised by Registers, InstructionId,
         auto option = (enable ? cs_opt_value.CS_OPT_ON : cs_opt_value.CS_OPT_OFF);
         cs_option(handle, cs_opt_type.CS_OPT_SKIPDATA, option).checkErrno;
     }
+    ///
+    unittest{
+        auto CODE = cast(ubyte[])"\x8d\x4c\x32\x08\x01\xd8\x81\xc6\x34\x12\x00\x00\x00\x91\x92";
+
+        auto cs = new Capstone!(Arch.x86)(ModeFlags(Mode.bit32));
+        auto instrs = cs.disasm(CODE, 0x1000);
+        assert(instrs.length == 3); // With skipdata disabled, disassembling will halt when encountering data
+        cs.skipData = true;
+        instrs = cs.disasm(CODE, 0x1000);
+        assert(instrs.length == 6);
+    }
 
     /** Customises behaviour in SKIPDATA mode of operation
      
@@ -360,6 +365,24 @@ class Capstone(Arch arch){ // Actually parametrised by Registers, InstructionId,
         auto setup = cs_opt_skipdata(this.mnemonic.ptr, this.callback ? &cCallback : null, &this.callback);
         cs_option(handle, cs_opt_type.CS_OPT_SKIPDATA_SETUP, cast(size_t)&setup).checkErrno;
     }
+    ///
+    unittest{
+        auto CODE = cast(ubyte[])"\x8d\x4c\x32\x08\x01\xd8\x81\xc6\x34\x12\x00\x00\x00\x91\x92";
+        // Custom data that can be referred to in a callback delegate
+        struct CallbackData{
+            int bytesToSkip;
+        }
+        auto myData = CallbackData(1);
+        size_t myCallback(in ubyte[] code, size_t offset) {
+            return myData.bytesToSkip++; // Always skip one more byte when encountering data
+        }
+
+        auto cs = new Capstone!(Arch.x86)(ModeFlags(Mode.bit32));
+        cs.skipData = true;                    // Enable skipdata mode
+        cs.setupSkipdata("db", &myCallback);   // Use custom callback, and "db" as custom mnemonic for data
+        const instrs = cs.disasm(CODE, 0x1000); // Disassemble (offsetting addresses by 0x1000)
+        assert(instrs.length == 6);
+    }
 
     // TODO: for system with scarce memory to be dynamically allocated such as OS kernel or firmware, the API cs_disasm_iter() might be a better choice than cs_disasm()
     /** Disassemble binary code, given the code buffer, address and number of instructions to be decoded
@@ -383,7 +406,18 @@ class Capstone(Arch arch){ // Actually parametrised by Registers, InstructionId,
 
         return instrAppnd.data;
     }
+    ///
+    unittest{
+        auto CODE = cast(ubyte[])"\x8d\x4c\x32\x08\x01\xd8\x81\xc6\x34\x12\x00\x00\x00\x91\x92";
 
+        auto cs = new Capstone!(Arch.x86)(ModeFlags(Mode.bit32)); // Initialise x86 32bit engine
+        auto res = cs.disasm(CODE, 0x1000);                       // Disassemble, offsetting addresses by 0x1000
+        assert("%s %s".format(res[0].mnemonic, res[0].opStr) == "lea ecx, dword ptr [edx + esi + 8]");
+        assert("%s %s".format(res[1].mnemonic, res[1].opStr) == "add eax, ebx");
+        assert("%s %s".format(res[2].mnemonic, res[2].opStr) == "add esi, 0x1234");
+    }
+
+    // TODO: Really needed? Almost identical to regular `toString`
     /** Determines user-friendly name of a register
     
     When in diet mode, this API is irrelevant because engine does not store register names
@@ -395,5 +429,15 @@ class Capstone(Arch arch){ // Actually parametrised by Registers, InstructionId,
         if(diet)
             throw new CapstoneException("Register names are not stored when running Capstone in diet mode", ErrorCode.IrrelevantDataAccessInDietEngine);
         return cs_reg_name(handle, regId).to!string;
+    }
+    ///
+    unittest{
+        import std.conv: to;
+
+        auto cs = new Capstone!(Arch.x86)(ModeFlags(Mode.bit32)); // Initialise x86 32bit engine
+        if(!diet){
+            assert(cs.regName(X86Register.eip) == X86Register.eip.to!string); // Mostly same output as `to!string`
+            assert(cs.regName(X86Register.st7) == "st(7)");                   // Differs sometimes though
+        }
     }
 }
